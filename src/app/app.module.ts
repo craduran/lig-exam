8e15188b533407c975f211f2081fb9c5f8a05d4a
craduran <craduran@gmail.com>
2020-02-12 13:13:27 +0800

    initializing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ListComponent } from './list/list.component';
+import { ListDetailComponent } from './list-detail/list-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    FooterComponent,
+    SidebarComponent,
+    ListComponent,
+    ListDetailComponent
   ],
   imports: [
     BrowserModule
